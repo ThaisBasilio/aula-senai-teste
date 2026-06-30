952f450ea44abaae5d546175b53ded85c30ef097
ThaisBasilio <0001272406@senaimgaluno.com.br>
2026-06-30 14:32:22 -0300

    Finaliza a implementação do Exercicio 01

--- Thais/Exercicio01/Program.cs
+++ Thais/Exercicio01/Program.cs
@@ -1,8 +1,15 @@
-﻿class Program{
+﻿using System;
+class Program{
 
     static void Main(){
+        double numero;
+        Console.Write("Digite um número: ");
+        numero = double.Parse(Console.ReadLine());
 
-        
+        if (numero > 20) { Console.WriteLine("Este número é maior que 20");}
+        else if (numero == 20) { Console.WriteLine("Este número é igual a 20");}
+        else { Console.WriteLine("Este número é menor que 20");}
+
     }
 
 }
