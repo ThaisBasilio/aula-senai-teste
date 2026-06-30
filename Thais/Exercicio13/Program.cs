4861598ce646e3bd82c672c90d9e7006e811f904
ThaisBasilio <0001272406@senaimgaluno.com.br>
2026-06-30 16:06:28 -0300

    Finalizado o Exercicio 13

---
+++ Thais/Exercicio13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program{
+    static void Main(){
+        double salario;
+        double prestacao;
+        Console.WriteLine("Digite o valor do salário: ");
+        salario = double.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o valor da prestação: ");
+        prestacao = double.Parse(Console.ReadLine());
+
+        if (prestacao > salario * 0.20) {Console.WriteLine("O empréstimo não pode ser concedido.");}
+        else {Console.WriteLine("O empréstimo pode ser concedido.");}
+            
+    }
+}
