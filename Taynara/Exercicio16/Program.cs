2e93c824074e66ccac82d4ee50b11037682ea564
itayvilela <lvenanciobrandao@gmail.com>
2026-07-01 14:28:58 -0300

    Finalizando o Exercicio16

---
+++ Taynara/Exercicio16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Exercicio16
+{
+    static void Main()
+    {
+        double sal=0;
+
+        Console.WriteLine("Digite o saldo: ");
+        sal = double.Parse(Console.ReadLine());
+
+        if (sal <= 10000000)
+        {
+            Console.WriteLine($"Com o {sal} o cliente é denominado VIP ");
+        }
+        else if (sal >= 500000 && sal <= 10000000)
+        {
+            Console.WriteLine($"Com o {sal} o cliente é taxado como MIDDLE ");
+        }
+        else
+        {
+            Console.WriteLine($"Com o {sal} o vliente será taxado como USUAL ");
+        }
+    }
+}
+
+
