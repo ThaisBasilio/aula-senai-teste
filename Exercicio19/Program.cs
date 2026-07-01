ac7e30f2b0a1828b26d0b5adec35870a3b116bc2
Vitor7509 <vitorcorinthians821@gmail.com>
2026-07-01 16:54:16 -0300

    Finalizada Exercicio 19

---
+++ Exercicio19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Exercicio_19
+{
+    static double PerguntaDouble(string texto)
+    {
+        Console.Write(texto);
+        return double.Parse(Console.ReadLine());
+    }
+    static void Main()
+    {
+        double salario;
+        double desconto;
+
+        salario = PerguntaDouble("Insira o salário do funcionário: ");
+        desconto = Math.Min(salario * 0.11, 334.29);
+
+        Console.WriteLine($"O salário final do funcionário será de {salario - desconto}, tendo um desconto de {desconto}.");
+    }
+}
